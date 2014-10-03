e8895e8c331a738a5a6b7498e2286ff27cae8edd
candyash <ashenafi.legehar@gmail.com>
2014-10-03 16:20:30 +0300

    app context added

--- db_upgrade.py
+++ db_upgrade.py
@@ -3,7 +3,7 @@ from migrate.versioning import api
 from config import ProductionConfig
 
 
-api.upgrade(ProductionConfig.SQLALCHEMY_DATABASE_URI, ProductionConfig,SQLALCHEMY_MIGRATE_REPO)
+api.upgrade(ProductionConfig.SQLALCHEMY_DATABASE_URI, ProductionConfig.SQLALCHEMY_MIGRATE_REPO)
 v = api.db_version(ProductionConfig.SQLALCHEMY_DATABASE_URI, ProductionConfig.SQLALCHEMY_MIGRATE_REPO)
 print('Current database version: ' + str(v))
 
