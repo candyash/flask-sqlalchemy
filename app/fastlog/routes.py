ccfc98909df3012b4b446e86b3b2c91c80b136b2
candyash <ashenafi.legehar@gmail.com>
2014-10-03 11:11:49 +0300

    try/except add

--- app/fastlog/routes.py
+++ app/fastlog/routes.py
@@ -8,16 +8,20 @@ from .forms import LoginForm
 
 @fastlog.route('/login', methods=['GET', 'POST'])
 def login():
-    if not current_app.config['DEBUG'] and not current_app.config['TESTING'] \
-            and not request.is_secure:
-        return redirect(url_for('.login', _external=True, _scheme='https'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user is None or not user.verify_password(form.password.data):
-            flash('Invalid email or password.')
-            return redirect(url_for('.login'))
-        login_user(user, form.remember_me.data)
+    try:
+        
+        if not current_app.config['DEBUG'] and not current_app.config['TESTING'] \
+                and not request.is_secure:
+            return redirect(url_for('.login', _external=True, _scheme='https'))
+        form = LoginForm()
+        if form.validate_on_submit():
+            user = User.query.filter_by(email=form.email.data).first()
+            if user is None or not user.verify_password(form.password.data):
+                flash('Invalid email or password.')
+                return redirect(url_for('.login'))
+            login_user(user, form.remember_me.data)
+    except:
+        flash("Error!")
         return redirect(request.args.get('next') or url_for('fast.index'))
     return render_template('fastlog/login.html', form=form)
 
