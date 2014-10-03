ddd7bd735d28c263055844a51f6a5802c1b48bb6
candyash <ashenafi.legehar@gmail.com>
2014-10-03 16:32:26 +0300

    app context added

--- db_create.py
+++ db_create.py
@@ -16,11 +16,14 @@ def initialize_database():
     except:
         flash ("database missing!!")
 
+try:
+    
+    if not os.path.exists(ProductionConfig.SQLALCHEMY_MIGRATE_REPO ):
+        api.create(ProductionConfig.SQLALCHEMY_MIGRATE_REPO, 'database_repository')
+        api.version_control(ProductionConfig.SQLALCHEMY_DATABASE_URI,ProductionConfig.SQLALCHEMY_MIGRATE_REPO)
+    else:
+        api.version_control(ProductionConfig.SQLALCHEMY_DATABASE_URI, ProductionConfig.SQLALCHEMY_MIGRATE_REPO,
+                            api.version(SQLALCHEMY_MIGRATE_REPO))
 
-if not os.path.exists(ProductionConfig.SQLALCHEMY_MIGRATE_REPO ):
-    api.create(ProductionConfig.SQLALCHEMY_MIGRATE_REPO, 'database_repository')
-    api.version_control(ProductionConfig.SQLALCHEMY_DATABASE_URI,ProductionConfig.SQLALCHEMY_MIGRATE_REPO)
-else:
-    api.version_control(ProductionConfig.SQLALCHEMY_DATABASE_URI, ProductionConfig.SQLALCHEMY_MIGRATE_REPO,
-                        api.version(SQLALCHEMY_MIGRATE_REPO))
-
+except DatabaseAlreadyControlledError:
+            raise Exception('Database already initialized')
