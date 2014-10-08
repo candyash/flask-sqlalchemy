570c25d58aaa187ebdc5eb84482ac6a7783780b7
candyash <ashenafi.legehar@gmail.com>
2014-10-09 00:53:00 +0300

    resource added

--- app/fastlog/routes.py
+++ app/fastlog/routes.py
@@ -24,7 +24,7 @@ def login():
             flash('Invalid email or password.')
             return redirect(url_for('.login'))
         login_user(user, form.remember_me.data)
-        return redirect(request.args.get('next') or url_for('fast.index'))
+        return redirect(url_for('fast.index'))
     return render_template('fastlog/login.html', form=form)
         
         
