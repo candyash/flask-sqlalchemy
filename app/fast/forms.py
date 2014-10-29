c8faa52c3e41087a699f8858df0c834ef6232db8
candyash <ashenafi.legehar@gmail.com>
2014-10-29 11:42:41 +0200

    Update forms.py

--- app/fast/forms.py
+++ app/fast/forms.py
@@ -11,16 +11,6 @@ class ProfileForm(Form):
     location=StringField('Location',validators=[Optional(),Length(1,30)] )
     bio=TextAreaField('Bio')
     submit=SubmitField('Submit')
-class PresenterCommentForm(Form):
-    body = PageDownField('Comment', validators=[Required()])
-    submit = SubmitField('Submit')
-class CommentForm(Form):
-    name = StringField('Name', validators=[Required(), Length(1, 64)])
-    email = StringField('Email', validators=[Required(), Length(1, 64),
-                                             Email()])
-    body = PageDownField('Comment', validators=[Required()])
-    notify = BooleanField('Notify when new comments are posted', default=True)
-    submit = SubmitField('Submit')
 class RegisterForm(Form):
     username=StringField('User Name', validators=[Required()])
     email = StringField('Email', validators=[Required(), Length(1, 64),
